314f73fe70ee9b0e5d1686b1249baea23d2861bd
David Lin <davi13a@gmail.com>
2021-04-04 23:52:36 +0800

    find good

--- read.py
+++ read.py
@@ -6,17 +6,26 @@ with open('reviews.txt','r') as f:
         ##count +=1
         ##if count % 1000 == 0:
             ##print(len(data))
-print('檔案讀取完了，總共有', len(data), '筆資料')
-print('----------------------')
-print(data[4])
+##print('檔案讀取完了，總共有', len(data), '筆資料')
+##print('----------------------')
+##print(data[4])
 
 sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
-print('留言的平均長度為',sum_len/len(data))
+##print('留言的平均長度為',sum_len/len(data))
 
 new = []
 for d in data:
     if len(d) < 100:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言中有good')
+print(good[2])
+print('----------------------')
+print(good[10])
